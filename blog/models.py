230bb542a04694ca97cbead6cf4d24ebc5d3dfec
Islam Assanov <islamassanov66@gmail.com>
2024-07-30 22:20:58 +0500

    Added posts database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    #passes the time now with current timezone
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    #if the user is deleted, we delete a post
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
